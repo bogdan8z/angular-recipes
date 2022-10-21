00e75e81a82189ba9386dbcecae2c4ad951414fb
bogdan g <bogdan g@>
2022-10-21 17:35:33 +0300

    add ingredient model; add insert shopping item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @Input() ingredient!:  Ingredient;
+  @Output() addEvent = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit(): void {
+    this.ingredient = new Ingredient('',0);
+  }
+
+  addNew(name: string, amount: number): void {
+    console.log('add2'+name);
+    this.addEvent.emit(new Ingredient(name, amount));    
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  selectedIngredient!: Ingredient | undefined;
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Oranges',2)
+  ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addIngredient(ingredient: Ingredient){
+    console.log('addingredient'+ ingredient);
+    this.ingredients.push(ingredient);
   }
 
 }
