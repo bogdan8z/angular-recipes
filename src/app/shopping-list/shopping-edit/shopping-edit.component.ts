b9f82db7555133b1cdb60621ed5cb8b1f221d053
bogdan g <bogdan g@>
2022-10-25 11:13:35 +0300

    send form to child in shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
-import {FormBuilder,  Validators} from '@angular/forms';
+import {FormBuilder,  FormGroup,  Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,43 +8,47 @@ import {FormBuilder,  Validators} from '@angular/forms';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @Input() ingredient!:  Ingredient;
+  @Input() ingredientForm : FormGroup;
   @Output() addEvent = new EventEmitter<Ingredient>();
   @Output() deleteEvent = new EventEmitter<Ingredient>();
 
-  editForm = this.formBuilder.group({
-    name: [null],
-    amount: [null]
-  });
-
+ 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.clearData();
+    // this.editForm = this.formBuilder.group({
+    //   name: [null],
+    //   amount: [null]
+    // });
+
+    
+    // this.ingredientForm.addControl(
+    //   'child',
+    //   new FormGroup(this.editForm.controls);
+    // );
   }
 
   clearData(){
-    this.ingredient = new Ingredient(null,null);
+    //this.ingredient = new Ingredient(null,null);
   }
 
   addNew(): void {    
-    const ingredient = this.editForm.value;   
-    this.addEvent.emit(new Ingredient(ingredient.name, ingredient.amount));    
-    this.editForm.reset();
+    const ingredient = this.ingredientForm.value;   
+    this.addEvent.emit(ingredient as Ingredient);    
+    this.ingredientForm.reset();
   }
 
   clear(){
-    const ingredient = this.editForm.value;   
-    console.log('clear' + ingredient.name);
-    this.editForm.reset();
-    
+    // const ingredient = this.editForm.value;   
+    // console.log('clear' + ingredient.name);
+    // this.editForm.reset();    
+    this.ingredientForm.reset();
   }
 
   delete(){
-    // if(!this.editForm.valid){
-    //   return;
-    // }
-    this.editForm.reset();
-    this.deleteEvent.emit();
+    var ing =  this.ingredientForm.value;
+    this.ingredientForm.reset();
+    this.deleteEvent.emit(ing as Ingredient);
   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,36 +8,42 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  selectedIngredient!: Ingredient | undefined;
+  itemEditForm: FormGroup;
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Oranges',2)
   ];
-  constructor() { }
 
+  constructor(private fb: FormBuilder) { } 
+  
+  
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(){
+    this.itemEditForm = this.fb.group({
+      amount: [null, [Validators.required]],
+      name: [null, [Validators.required]]
+    });
+    
   }
 
   addIngredient(ingredient: Ingredient){    
-    
-    const idx = this.ingredients.findIndex(ing => ing.amount === this.selectedIngredient.amount && ing.name === this.selectedIngredient.name);
-    console.log('addingredient'+ idx);
+    const idx = this.ingredients.findIndex(ing => ing.amount === ingredient.amount && ing.name === ingredient.name);    
+    if(idx>-1){
+      return;
+    }
     this.ingredients.push(ingredient);
   }
 
   onSelect(ingredient: Ingredient){
-    this.selectedIngredient = ingredient;    
-    console.log('onselect'+ingredient.name);
+    this.itemEditForm.patchValue({name: ingredient.name, amount: ingredient.amount});
   }
 
-  deleteIngredient(){    
-    if(!this.selectedIngredient){
-      return;
-    }
-
-    const idx = this.ingredients.findIndex(ing => ing.amount === this.selectedIngredient.amount && ing.name === this.selectedIngredient.name);
+  deleteIngredient(ingredient: Ingredient){    
+    console.log('dellll'+ingredient.name);
+    const idx = this.ingredients.findIndex(ing => ing.amount === ingredient.amount && ing.name === ingredient.name);
     this.ingredients.splice(idx, 1);
-    this.selectedIngredient = undefined;
   }
-
 }
